0c33a608a8ade7259df505b038c1e092b7d6e3a4
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-16 12:25:52 +0530

    Update config.py

--- Naruto/config/config.py
+++ Naruto/config/config.py
@@ -1,7 +1,7 @@
 # API credentials
 API_ID = "19099900"
 API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
-BOT_TOKEN = "your_bot_token"
+BOT_TOKEN = "6206599982:AAGMtdPmgw7wRF5EsdREiJT3Qb9Yy8ALIVE"
 
 # Database configuration
 MONGO_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"
