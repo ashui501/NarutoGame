11e5f7f5fb8d14e0a534a974ba2c810728c02b32
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-16 12:07:16 +0530

    Update config.py

--- Naruto/config/config.py
+++ Naruto/config/config.py
@@ -1,1 +1,12 @@
+# API credentials
+API_ID = "your_api_id"
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# Database configuration
+MONGO_URI = "mongodb://localhost:27017/"
+DB_NAME = "naruto_bot_db"
+
+# Download directory for temporary files
+DOWNLOAD_DIRECTORY = "./"
 
