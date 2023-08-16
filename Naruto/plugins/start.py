51de0c2049fb9b716cb587f4b48dbea19b892539
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-16 16:42:48 +0530

    Update start.py

--- Naruto/plugins/start.py
+++ Naruto/plugins/start.py
@@ -3,26 +3,23 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Naruto.database.database import add_user, get_user
 from Naruto.config.config import SUPPORT_CHAT
 
-welcome_back_message = "Welcome back, {}!"
-
 @Client.on_message(filters.command("start"))
 async def start_command(client, message):
     user_id = message.from_user.id
     username = message.from_user.username
-    existing_user = get_user(user_id, username)
+    existing_user = get_user(user_id)
     
     if not existing_user:
         add_user(user_id, username)
-        await message.reply("Welcome to Naruto Bot! You've been added to our database.")
+        await message.reply("Welcome to Naruto Game Bot!")
     else:
-        await message.reply(welcome_back_message.format(username))
-
-        # Create an inline keyboard with a button to contact support
+        welcome_back_message = f"Welcome back in Naruto Game Bot, {username}!\n\n" \
+                               f"Click the button below to join our support chat."
+        
         inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Contact Support", url=f"https://t.me/{SUPPORT_CHAT}")]
+            [InlineKeyboardButton("Join Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")]
         ])
         
         await message.reply_text(welcome_back_message, reply_markup=inline_keyboard)
 
 # You can add more commands or functionality to this plugin as needed
-
