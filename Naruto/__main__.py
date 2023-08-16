1534de9e758bb333dc5dbf79004282c9f1c609b9
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-16 16:44:25 +0530

    Update __main__.py

--- Naruto/__main__.py
+++ Naruto/__main__.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     # Send a message to the support chat upon bot startup
     try:
         with app:
-            app.send_message(SUPPORT_CHAT, "Naruto Bot has started!")
+            app.send_message(SUPPORT_CHAT, "Naruto Bot has started!\n Dev: @SexyNano")
     except Exception as e:
         print(f"Error sending startup message: {e}")
 
