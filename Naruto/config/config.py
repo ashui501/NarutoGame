5fac1810852cff24d7457a6a030568f5a33987fd
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-17 08:07:27 +0530

    Update config.py

--- Naruto/config/config.py
+++ Naruto/config/config.py
@@ -1,11 +1,11 @@
 # API credentials
-API_ID = "19099900"
-API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
+API_ID = "24545226"
+API_HASH = "c9cc641ca738adf1ea5b29f3dd99315d"
 BOT_TOKEN = "6206599982:AAGMtdPmgw7wRF5EsdREiJT3Qb9Yy8ALIVE"
 
 # Database configuration
 MONGO_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = "naruto_bot_db"
+DB_NAME = "naruto_game_db"
 
 # Download directory for temporary files
 DOWNLOAD_DIRECTORY = "./"
